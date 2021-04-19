266a2ef83f7c7a540f8b96a023ad45095dab11fa
thomaseikhaug <thomaseikhaug@thomas-mbp.home>
2021-04-19 18:40:53 +0200

    HashOf

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -12,55 +12,69 @@ import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Hash { 
-	
-	private static BigInteger hashint; 
-	
-	public static BigInteger hashOf(String entity) {		
-		
+public class Hash {
+
+	private static BigInteger hashint;
+
+	public static BigInteger hashOf(String entity) {
+
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
+
 		// we use MD5 with 128 bits digest
-		
+
 		// compute the hash of the input 'entity'
-		
+
 		// convert the hash into hex format
-		
+
 		// convert the hex into BigInteger
-		
+
 		// return the BigInteger
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
 		
+			byte[] entityBytes = entity.getBytes();
+			md.update(entityBytes);
+			byte[] digest = md.digest();
+			
+			String value = toHex(digest);
+			
+			hashint = new BigInteger(value, 16);
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
 		return hashint;
 	}
-	
+
 	public static BigInteger addressSize() {
-		
+
 		// Task: compute the address size of MD5
-		
+
 		// get the digest length
-		
+
 		// compute the number of bits = digest length * 8
-		
+
 		// compute the address size = 2 ^ number of bits
-		
+
 		// return the address size
-		
 		return null;
 	}
-	
+
 	public static int bitSize() {
-		
+
 		int digestlen = 0;
-		
+
 		// find the digest length
-		
-		return digestlen*8;
+
+		return digestlen * 8;
 	}
-	
+
 	public static String toHex(byte[] digest) {
 		StringBuilder strbuilder = new StringBuilder();
-		for(byte b : digest) {
-			strbuilder.append(String.format("%02x", b&0xff));
+		for (byte b : digest) {
+			strbuilder.append(String.format("%02x", b & 0xff));
 		}
 		return strbuilder.toString();
 	}
