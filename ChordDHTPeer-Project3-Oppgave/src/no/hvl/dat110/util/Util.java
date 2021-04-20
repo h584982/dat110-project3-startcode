bec126a2320ece297e72996e5dc20e96179ae1db
Simen Gjerde <31205791+h181226@users.noreply.github.com>
2021-04-20 09:21:34 +0200

    Update Util.java

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Util.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Util.java
@@ -44,6 +44,19 @@ public class Util {
 		// implement: read the descriptions above
 		boolean cond = false;
 
+		if (lower.compareTo(upper) < 0) {
+			if(id.compareTo(lower) >= 0 && id.compareTo(upper) <= 0) {
+				cond = true;
+			}
+		}else if(lower.compareTo(upper) > 0) {
+			if(id.compareTo(lower) >= 0 && id.compareTo(Hash.addressSize()) <= 0 || id.compareTo(new BigInteger("0")) >= 0 && id.compareTo(upper) <= 0) {
+				cond = true;
+			}
+		}else {
+			if(id.compareTo(lower) == 0) {
+				cond = true;
+			}
+		}
 		
 		return cond;
 	}
